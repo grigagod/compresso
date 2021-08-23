325ac242badea1dfe0e241300f99973501e818d7
Gregory <podkidysh2002@gmail.com>
2021-08-23 08:27:36 +0300

    Implement jwt util functions

---
+++ pkg/utils/jwt.go
@@ -0,0 +1,76 @@
+package utils
+
+import (
+	"errors"
+	"html"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+)
+
+// JWT Claims struct
+type Claims struct {
+	ID uuid.UUID `json:"id"`
+	jwt.StandardClaims
+}
+
+// GenerateJWTToken generates new JWT Token with claims, which includes the id and expiry time.
+func GenerateJWTToken(id uuid.UUID, expires time.Duration, JwtSecretKey string) (string, error) {
+	// Register the JWT claims, which includes the id and expiry time
+	claims := &Claims{
+		ID: id,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(expires).Unix(),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Register the JWT string
+	tokenString, err := token.SignedString([]byte(JwtSecretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+// ExtractJWTFromRequest extracts JWT claims form request.
+func ExtractJWTFromRequest(r *http.Request, JwtSecretKey string) (*Claims, error) {
+	// Get the JWT string
+	tokenString := ExtractBearerToken(r)
+
+	// Parse the JWT string
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(JwtSecretKey), nil
+	})
+
+	if err != nil {
+		if errors.Is(err, jwt.ErrSignatureInvalid) {
+			return nil, errors.New("invalid token signature")
+		}
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, nil
+}
+
+// ExtractBearerToken extracts bearer token from request Authorization header.
+func ExtractBearerToken(r *http.Request) string {
+	headerAuthorization := r.Header.Get("Authorization")
+	bearerToken := strings.Split(headerAuthorization, " ")
+	return html.EscapeString(bearerToken[1])
+}
