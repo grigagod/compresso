396fee9eb098d898890e05ffc0d9cca67217b182
Gregory <podkidysh2002@gmail.com>
2021-08-24 10:24:34 +0300

    Add handlers method check

--- internal/auth/delivery/http/routes.go
+++ internal/auth/delivery/http/routes.go
@@ -3,6 +3,6 @@ package http
 import "github.com/gorilla/mux"
 
 func MapAuthRoutes(router *mux.Router, h *authHandlers) {
-	router.HandleFunc("/register", h.Register())
-	router.HandleFunc("/login", h.Login())
+	router.HandleFunc("/register", h.Register()).Methods("POST")
+	router.HandleFunc("/login", h.Login()).Methods("POST")
 }
