f866407932f9ee69ab2f7cd0e41da320a2a016ed
Gregory <podkidysh2002@gmail.com>
2021-08-31 15:36:11 +0300

    Fix sql queries

--- internal/auth/repo/sql_queries.go
+++ internal/auth/repo/sql_queries.go
@@ -1,7 +1,7 @@
 package repo
 
 const (
-	createUserQuery = `INSERT INTO auth.users(user_id, username, password, created_at) VALUES($1, $2, $3, $4) RETURNING *`
+	createUserQuery = `INSERT INTO svc.users(user_id, username, password, created_at) VALUES($1, $2, $3, $4) RETURNING *`
 
-	findUserByNameQuery = `SELECT * FROM auth.users WHERE username = $1`
+	findUserByNameQuery = `SELECT * FROM svc.users WHERE username = $1`
 )
