3da71322e3327bc8d8a1a3ad2a3847bf97ac6614
Gregory <podkidysh2002@gmail.com>
2021-09-07 09:58:35 +0300

    Add rmq(RabbitMQ) pkg basic implementation

---
+++ pkg/rmq/integration_test.go
@@ -0,0 +1,47 @@
+package rmq
+
+import (
+	"testing"
+
+	"github.com/streadway/amqp"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPublisherConsumerExchange(t *testing.T) {
+	conf := Config{
+		Proto: "amqp://",
+		Host:  "127.0.0.1",
+		Port:  "5672",
+	}
+
+	ch, err := NewChannelFromConfig(&conf)
+	assert.Nil(t, err)
+	_, err = ch.QueueDeclare("test_queue", true, false, false, false, amqp.Table{})
+	assert.Nil(t, err)
+
+	pub := NewPublisher(ch)
+	err = pub.Send(NewMessage(amqp.Publishing{
+		Headers:     map[string]interface{}{},
+		ContentType: PlainContentType,
+		Body:        []byte("test1234"),
+	}, &QueueWriteConfig{
+		Name:      "test_queue",
+		Mandatory: true,
+	}))
+	assert.Nil(t, err)
+
+	cn := NewConsumer(ch)
+	delivery, err := cn.Receiving(&QueueReadConfig{
+		Name:    "test_queue",
+		Durable: true,
+	})
+	assert.Nil(t, err)
+
+	for msg := range delivery {
+		err = msg.Ack(false)
+		assert.Nil(t, err)
+		break
+	}
+	err = ch.Close()
+	assert.Nil(t, err)
+}
---
+++ pkg/rmq/publisher.go
@@ -0,0 +1,45 @@
+package rmq
+
+import "github.com/streadway/amqp"
+
+// RMQ constants
+const (
+	JSONContentType  = "application/json"
+	PlainContentType = "text/plain"
+)
+
+// Message contains message and config.
+type Message struct {
+	Publishing amqp.Publishing
+	Config     *QueueWriteConfig
+}
+
+// NewMessage return new message.
+func NewMessage(message amqp.Publishing, queueConfig *QueueWriteConfig) Message {
+	return Message{
+		Publishing: message,
+		Config:     queueConfig,
+	}
+}
+
+// Publisher describe publisher.
+type Publisher struct {
+	Channel *Channel
+}
+
+// NewPublisher return new publisher.
+func NewPublisher(ch *Channel) *Publisher {
+	return &Publisher{
+		Channel: ch,
+	}
+}
+
+// Send send message to rmq.
+func (c *Publisher) Send(message Message) error {
+	return c.Channel.Publish(
+		message.Config.Exchange,
+		message.Config.Name,
+		message.Config.Mandatory,
+		message.Config.Immediate,
+		message.Publishing)
+}
