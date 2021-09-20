064dd4a89854e669497c46683b415078bbdf1346
Gregory Podkidysh <grishapod228@gmail.com>
2021-09-20 10:13:38 +0300

    Remove http server LoadConfig()

--- internal/server/config.go
+++ internal/server/config.go
@@ -1,10 +1,7 @@
 package server
 
 import (
-	"log"
 	"time"
-
-	"github.com/grigagod/compresso/pkg/utils"
 )
 
 // Config stores http server config.
@@ -14,21 +11,4 @@ type Config struct {
 	WriteTimeout time.Duration
 	ReadTimeout  time.Duration
 	IdleTimeout  time.Duration
-}
-
-func LoadConfig(filepath string) (*Config, error) {
-	v, err := utils.LoadConfig(filepath)
-	if err != nil {
-		log.Printf("unable to load auth config, %v", err)
-	}
-
-	var c Config
-
-	err = v.Unmarshal(&c)
-	if err != nil {
-		log.Printf("unable to decode into struct, %v", err)
-		return nil, err
-	}
-
-	return &c, nil
 }
