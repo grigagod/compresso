7052ad62d8f5b1ea63bce04c863b4bd202d3202e
Gregory Podkidysh <grishapod228@gmail.com>
2021-10-01 14:29:33 +0300

    Update db schema

--- migrations/1_create_tables.up.sql
+++ migrations/1_create_tables.up.sql
@@ -12,24 +12,33 @@ CREATE TABLE IF NOT EXISTS svc.users (
 
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'image_format') THEN
-        CREATE TYPE image_format AS ENUM('jpg', 'png');
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'video_format') THEN
+        CREATE TYPE image_format AS ENUM('mkv', 'webm');
     END IF;
-    
+
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'processing') THEN
         CREATE TYPE processing_state as ENUM('queued', 'processing', 'done', 'failed');
     END IF;
 END$$;
 
---CREATE TYPE IF NOT EXISTS image_format as ENUM('jpg', 'png');
 
-CREATE TABLE IF NOT EXISTS svc.image_tickets (
-    ticket_id     UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+CREATE TABLE IF NOT EXISTS svc.user_videos (
+    video_id      UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     author_id     UUID NOT NULL REFERENCES svc.users (user_id),
-    format        image_format NOT NULL,
-    state         processing_state NOT NULL,
-    compression   SMALLINT NOT NULL,
-    origin_url    TEXT NOT NULL,
-    processed_url TEXT,
+    format        video_format NOT NULL,
+    url           TEXT NOT NULL,
     created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS svc.video_tickets (
+    ticket_id     UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    video_id      UUID NOT NULL REFERENCES svc.user_videos (video_id),
+    author_id     UUID NOT NULL REFERENCES svc.users (user_id),
+    state         processing_state NOT NULL,
+    format        video_format NOT NULL,
+    crf           SMALLINT NOT NULL,
+    url           TEXT,
+    created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
