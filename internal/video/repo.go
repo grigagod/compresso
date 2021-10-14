00f56ebdc5751eafa773d5b8288261d4ffdfd0db
Gregory Podkidysh <grishapod228@gmail.com>
2021-10-14 15:47:25 +0300

    Implement video svc repo

--- internal/video/repo.go
+++ internal/video/repo.go
@@ -3,11 +3,14 @@ package video
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/grigagod/compresso/internal/models"
 )
 
 type Repository interface {
-	Create(ctx context.Context, video *models.Video) (*models.Video, error)
+	CreateVideo(ctx context.Context, video *models.Video) (*models.Video, error)
 	CreateTicket(ctx context.Context, ticket *models.VideoTicket) (*models.VideoTicket, error)
-	UpdateTicket(ctx context.Context, ticket *models.VideoTicket) error
+	UpdateTicket(ctx context.Context, ticket *models.VideoTicket) (*models.VideoTicket, error)
+	FindVideoByID(ctx context.Context, id uuid.UUID) (*models.Video, error)
+	FindTicketByID(ctx context.Context, id uuid.UUID) (*models.VideoTicket, error)
 }
--- internal/video/repo/repo.go
+++ internal/video/repo/repo.go
@@ -1,13 +1,15 @@
-package video
+package repo
 
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/grigagod/compresso/internal/models"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 )
 
+// VideoRepo implement video.Repository interface.
 type VideoRepo struct {
 	db *sqlx.DB
 }
@@ -18,14 +20,15 @@ func NewVideoRepository(db *sqlx.DB) *VideoRepo {
 	}
 }
 
-func (r *VideoRepo) Create(ctx context.Context, video *models.Video) (*models.Video, error) {
+// CreateVideo insert video model in db.
+func (r *VideoRepo) CreateVideo(ctx context.Context, video *models.Video) (*models.Video, error) {
 	query := `INSERT INTO
-        svc.user_videos(author_id, format, url, created_at)
-        VALUES($1, $2, $3, now()) RETURNING *`
+        svc.user_videos(video_id, author_id, format, url, created_at)
+        VALUES($1, $2, $3, $4, now()) RETURNING *`
 
 	var v models.Video
 
-	err := r.db.QueryRowxContext(ctx, query, &video.AuthorID, &video.Format, &video.URL).StructScan(&v)
+	err := r.db.QueryRowxContext(ctx, query, &video.ID, &video.AuthorID, &video.Format, &video.URL).StructScan(&v)
 	if err != nil {
 		return nil, errors.Wrap(err, "VideoRepo.Create.StructScan")
 	}
@@ -33,35 +36,68 @@ func (r *VideoRepo) Create(ctx context.Context, video *models.Video) (*models.Vi
 	return &v, nil
 }
 
+// CreateTicket insert video ticket model in DB.
 func (r *VideoRepo) CreateTicket(ctx context.Context, ticket *models.VideoTicket) (*models.VideoTicket, error) {
 	query := `INSERT INTO
-        svc.video_tickets(video_id, author_id, format, state, crf, url, created_at)
-        VALUES($1, $2, $3, $4, $5, $6, COALESCE(NULLIF($8, ''), $8), now()) RETURNING *`
+        svc.video_tickets(ticket_id, video_id, author_id, target_format, state, crf, url, created_at)
+        VALUES($1, $2, $3, $4, $5, $6, COALESCE(NULLIF($7, ''), $7), now()) RETURNING *`
 
 	var t models.VideoTicket
 
-	err := r.db.QueryRowxContext(ctx, query, &ticket.VideoID, &t.AuthorID, &ticket.Format,
+	err := r.db.QueryRowxContext(ctx, query, &ticket.ID, &ticket.VideoID, &ticket.AuthorID, &ticket.TargetFormat,
 		&ticket.State, &ticket.CRF, &ticket.URL).StructScan(&t)
 	if err != nil {
 		return nil, errors.Wrap(err, "VideoRepo.Create.StructScan")
 
 	}
+
 	return &t, nil
 }
 
+// UpdateTicket update video ticket model in DB.
 func (r *VideoRepo) UpdateTicket(ctx context.Context, ticket *models.VideoTicket) (*models.VideoTicket, error) {
 	query := `UPDATE svc.video_tickets
-        SET format = $1,
+        SET target_format = $1,
             state  = $2,
-            url = COALESCE(NULLIF($3, ''), $3
-        WHERE ticket_id = $4 AND author_id = $5`
+            url = COALESCE(NULLIF($3, ''), $3)
+        WHERE ticket_id = $4
+        RETURNING *
+    `
 
 	var t models.VideoTicket
 
-	err := r.db.GetContext(ctx, &t, query, &ticket.Format, &ticket.State, &ticket.URL,
-		&ticket.ID, &ticket.AuthorID)
+	err := r.db.GetContext(ctx, &t, query, &ticket.TargetFormat, &ticket.State, &ticket.URL,
+		&ticket.ID)
 	if err != nil {
 		return nil, errors.Wrap(err, "VideoRepo.Update.GetContext")
+	}
+
+	return &t, nil
+}
+
+// FindVideoByID select by ID video model form DB.
+func (r *VideoRepo) FindVideoByID(ctx context.Context, id uuid.UUID) (*models.Video, error) {
+	query := `SELECT * FROM svc.user_videos WHERE video_id = $1`
+
+	var v models.Video
+
+	err := r.db.QueryRowxContext(ctx, query, id).StructScan(&v)
+	if err != nil {
+		return nil, errors.Wrap(err, "VideoRepo.FindVideoByID.SelectContext")
+	}
+
+	return &v, nil
+}
+
+// FindTicketByID select by ID video ticket model from DB.
+func (r *VideoRepo) FindTicketByID(ctx context.Context, id uuid.UUID) (*models.VideoTicket, error) {
+	query := `SELECT * FROM svc.video_tickets WHERE ticket_id = $1`
+
+	var t models.VideoTicket
+
+	err := r.db.SelectContext(ctx, &t, query, id)
+	if err != nil {
+		return nil, errors.Wrap(err, "VideoRepo.FindTicketByID.SelectContext")
 	}
 
 	return &t, nil
