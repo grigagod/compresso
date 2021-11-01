52ce1c0e65da5be073562193bab39bfafdbb3370
Gregory Podkidysh <grishapod228@gmail.com>
2021-11-01 07:46:55 +0300

    Expand video repository interface

--- internal/video/repo.go
+++ internal/video/repo.go
@@ -11,6 +11,8 @@ type Repository interface {
 	CreateVideo(ctx context.Context, video *models.Video) (*models.Video, error)
 	CreateTicket(ctx context.Context, ticket *models.VideoTicket) (*models.VideoTicket, error)
 	UpdateTicket(ctx context.Context, ticket *models.VideoTicket) (*models.VideoTicket, error)
-	FindVideoByID(ctx context.Context, id uuid.UUID) (*models.Video, error)
-	FindTicketByID(ctx context.Context, id uuid.UUID) (*models.VideoTicket, error)
+	GetVideoByID(ctx context.Context, id uuid.UUID) (*models.Video, error)
+	GetTicketByID(ctx context.Context, id uuid.UUID) (*models.VideoTicket, error)
+	GetVideos(ctx context.Context, authorID uuid.UUID) ([]*models.Video, error)
+	GetTickets(ctx context.Context, authorID uuid.UUID) ([]*models.VideoTicket, error)
 }
--- internal/video/repo/repo.go
+++ internal/video/repo/repo.go
@@ -74,30 +74,56 @@ func (r *VideoRepo) UpdateTicket(ctx context.Context, ticket *models.VideoTicket
 	return &t, nil
 }
 
-// FindVideoByID select by ID video model form DB.
-func (r *VideoRepo) FindVideoByID(ctx context.Context, id uuid.UUID) (*models.Video, error) {
-	query := `SELECT * FROM svc.user_videos WHERE video_id = $1`
+// GetVideoByID select by ID video model form DB.
+func (r *VideoRepo) GetVideoByID(ctx context.Context, authorID uuid.UUID, id uuid.UUID) (*models.Video, error) {
+	query := `SELECT * FROM svc.user_videos WHERE author_id = $1 AND video_id = $2`
 
 	var v models.Video
 
-	err := r.db.QueryRowxContext(ctx, query, id).StructScan(&v)
+	err := r.db.QueryRowxContext(ctx, query, authorID, id).StructScan(&v)
 	if err != nil {
-		return nil, errors.Wrap(err, "VideoRepo.FindVideoByID.SelectContext")
+		return nil, errors.Wrap(err, "VideoRepo.GetVideoByID.StructScan")
 	}
 
 	return &v, nil
 }
 
-// FindTicketByID select by ID video ticket model from DB.
-func (r *VideoRepo) FindTicketByID(ctx context.Context, id uuid.UUID) (*models.VideoTicket, error) {
-	query := `SELECT * FROM svc.video_tickets WHERE ticket_id = $1`
+// GetTicketByID select by ID video ticket model from DB.
+func (r *VideoRepo) GetTicketByID(ctx context.Context, authorID, id uuid.UUID) (*models.VideoTicket, error) {
+	query := `SELECT * FROM svc.video_tickets WHERE authorID =$1 AND ticket_id = $2`
 
 	var t models.VideoTicket
 
-	err := r.db.SelectContext(ctx, &t, query, id)
+	err := r.db.QueryRowxContext(ctx, query, authorID, id).StructScan(&t)
 	if err != nil {
-		return nil, errors.Wrap(err, "VideoRepo.FindTicketByID.SelectContext")
+		return nil, errors.Wrap(err, "VideoRepo.GetTicketByID.StructScan")
 	}
 
 	return &t, nil
+}
+
+// GetVideos select videos by author ID.
+func (r *VideoRepo) GetVideos(ctx context.Context, authorID uuid.UUID) ([]*models.Video, error) {
+	query := `SELECT * FROM svc.user_videos WHERE author_id = $1`
+	var videos []*models.Video
+
+	err := r.db.SelectContext(ctx, videos, query, authorID)
+	if err != nil {
+		return nil, errors.Wrap(err, "VideoRepo.GetVideos.SelectContext")
+	}
+
+	return videos, nil
+}
+
+// GetTickets select tickets by author ID.
+func (r *VideoRepo) GetTickets(ctx context.Context, authorID uuid.UUID) ([]*models.VideoTicket, error) {
+	query := `SELECT * FROM svc.video_tickets WHERE author_id = $1`
+	var tickets []*models.VideoTicket
+
+	err := r.db.SelectContext(ctx, tickets, query, authorID)
+	if err != nil {
+		return nil, errors.Wrap(err, "VideoRepo.GetTickets.SelectContext")
+	}
+
+	return tickets, nil
 }
