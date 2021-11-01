54acbefd43cee35b8d108f66e49dd307189fa3df
Gregory Podkidysh <grishapod228@gmail.com>
2021-11-01 08:28:36 +0300

    Refactor and expand video usecases

--- internal/video/repo.go
+++ internal/video/repo.go
@@ -8,11 +8,11 @@ import (
 )
 
 type Repository interface {
-	CreateVideo(ctx context.Context, video *models.Video) (*models.Video, error)
-	CreateTicket(ctx context.Context, ticket *models.VideoTicket) (*models.VideoTicket, error)
+	InsertVideo(ctx context.Context, video *models.Video) (*models.Video, error)
+	InsertTicket(ctx context.Context, ticket *models.VideoTicket) (*models.VideoTicket, error)
 	UpdateTicket(ctx context.Context, ticket *models.VideoTicket) (*models.VideoTicket, error)
-	GetVideoByID(ctx context.Context, id uuid.UUID) (*models.Video, error)
-	GetTicketByID(ctx context.Context, id uuid.UUID) (*models.VideoTicket, error)
-	GetVideos(ctx context.Context, authorID uuid.UUID) ([]*models.Video, error)
-	GetTickets(ctx context.Context, authorID uuid.UUID) ([]*models.VideoTicket, error)
+	SelectVideoByID(ctx context.Context, authorID, id uuid.UUID) (*models.Video, error)
+	SelectTicketByID(ctx context.Context, authorID, id uuid.UUID) (*models.VideoTicket, error)
+	SelectVideos(ctx context.Context, authorID uuid.UUID) ([]*models.Video, error)
+	SelectTickets(ctx context.Context, authorID uuid.UUID) ([]*models.VideoTicket, error)
 }
--- internal/video/usecase.go
+++ internal/video/usecase.go
@@ -4,10 +4,15 @@ import (
 	"context"
 	"io"
 
+	"github.com/google/uuid"
 	"github.com/grigagod/compresso/internal/models"
 )
 
 type UseCase interface {
-	UploadVideo(ctx context.Context, video *models.Video, file io.Reader) (*models.Video, error)
+	CreateVideo(ctx context.Context, video *models.Video, file io.Reader) (*models.Video, error)
 	CreateTicket(ctx context.Context, ticket *models.VideoTicket) (*models.VideoTicket, error)
+	GetVideoByID(ctx context.Context, authorID, id uuid.UUID) (*models.Video, error)
+	GetTicketByID(ctx context.Context, authorID, id uuid.UUID) (*models.VideoTicket, error)
+	GetVideos(ctx context.Context, authorID uuid.UUID) ([]*models.Video, error)
+	GetTickets(ctx context.Context, authorID uuid.UUID) ([]*models.VideoTicket, error)
 }
